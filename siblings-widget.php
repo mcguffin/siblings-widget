fe391495a77f6ca034b0cb7a1d979bb81b77202b
mcguffin <joern@flyingletters.com>
2018-01-18 17:41:21 +0100

    fix constructor

--- siblings-widget.php
+++ siblings-widget.php
@@ -5,7 +5,7 @@ Plugin Name: Page Siblings Widget
 Plugin URI: https://github.com/mcguffin/siblings-widget/
 Description: Widget showing a menu with all the siblings of the current selected page.
 Author: Jörn Lund
-Version: 0.0.2
+Version: 0.0.3
 Author URI: https://github.com/mcguffin/
 
 Text Domain: siblings
@@ -19,9 +19,9 @@ class Silings_Widget extends WP_Widget {
 		'title'			=> '',
 		'use_tree_title'	=> true,
 		'show_branch'	=> 'top', // 'top' | 'current'
-		'depth'			=> 0, // flat. 
+		'depth'			=> 0, // flat.
 	);
-	function Silings_Widget() {
+	function __construct() {
 		$widget_ops = array('classname' => 'siblings_widget', 'description' => __('Shows siblings of the current selected page.','siblings'));
 		parent::__construct('siblings_widget',__('Page Sibling','siblings') , $widget_ops);
 	}
@@ -31,18 +31,18 @@ class Silings_Widget extends WP_Widget {
 
 		if ( $post && ($post->post_parent or $this->page_has_children($post->ID) ) ) {
 			$parent_post = $post;
-			
+
 			// show branch
 			if ( $show_branch == 'top' )
 				while ( ($parent_post = $this->get_parent_post($parent_post)) && $parent_post->post_parent );
 			else if ( $show_branch == 'current' )
 				$parent_post = get_post( $post->post_parent );
-			
+
 			$func = create_function('$args','$args[\'child_of\'] = '.$parent_post->ID.';$args[\'show_home\']=false;return $args;' );
-			
+
 			// exclude non-hierarchical pages as well
 			add_filter( 'wp_page_menu_args', $func );
-		
+
 			$args = array(
 				'depth'       => $depth,
 				'sort_column' => 'menu_order',
@@ -64,7 +64,7 @@ class Silings_Widget extends WP_Widget {
 			echo $after_widget;
 			remove_filter( 'wp_page_menu_args', $func );
 		}
-		
+
 	}
 	function update( $new_instance , $old_instance ) {
 		return $new_instance;
@@ -83,8 +83,8 @@ class Silings_Widget extends WP_Widget {
 				<input type="radio" value="0" id="<?php echo $this->get_field_id( 'use_tree_title' ); ?>" name="<?php echo $this->get_field_name( 'use_tree_title' ); ?>" <?php checked( $use_tree_title, 0 );?>>
 				<input type="text" name="<?php echo $this->get_field_name( 'title' ); ?>" id="<?php echo $this->get_field_id( 'title' ); ?>" value="<?php echo esc_attr($title) ?>" />
 			</label><?php
-			
-			
+
+
 		?></p><?php
 	}
 	private function page_has_children( $post_ID ) {
@@ -111,7 +111,7 @@ function pagesiblings_children( $args ) {
 	// which page are we on.
 	global $post;
 	$old_post = $post;
-	
+
 	$children = get_posts( "post_type={$post->post_type}&post_parent={$post->ID}" );
 	$custom_query = new WP_Query( array(
 		'post_type' => 'page',
